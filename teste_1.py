e387290c072ce11b07b3361e8008852909135071
felipegdm <felipegmelo@usp.br>
2019-05-29 11:12:17 -0300

    Adjust teste_1

--- teste_1.py
+++ teste_1.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     def F(t, x):
         return 1 + np.power((x - t), 2)
 
-    x_0 = -18.95
+    x_0 = np.array([-18.95])
     t_0 = 1.05
     t_f = 3
-    output = runge_kutta(F, x_0, 0.01, t_0, t_f)
-    plt.plot(output[:, 1], output[:, 0], 'b.')
+    output, ts = runge_kutta(F, x_0, 0.01, t_0, t_f)
+    plt.plot(ts, output, 'b.')
     plt.show()
